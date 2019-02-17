b5618ede523a33472d1e48fd9f34d7241a4ae5fd
nb20102010 <lirunliruntc@gmail.com>
2019-02-17 08:46:45 -0700

    c02

---
+++ src/c02_TestFrame/TestJunit1.java
@@ -0,0 +1,19 @@
+package c02_TestFrame;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestJunit1 {
+	
+	String message = "Robert";
+	MessageUtil messageUtil = new MessageUtil(message);
+	
+	@Test
+	public void testPrintMessage() {
+		System.out.println("Inside testPrintMessage()");
+		assertEquals(message, messageUtil.printMessage());
+	}
+	
+}
+
